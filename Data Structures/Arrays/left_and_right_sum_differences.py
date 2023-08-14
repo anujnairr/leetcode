d1176970a613409c172c111257d0b95f9fd55f9f
Nair, Anuj <anujnairr@gmail.com>
2023-08-15 00:03:18 +0530

    First commit

---
+++ Data Structures/Arrays/left_and_right_sum_differences.py
@@ -0,0 +1,14 @@
+# Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+# answer.length == nums.length.
+# answer[i] = |leftSum[i] - rightSum[i]|.
+# leftSum[i] is the sum of elements to the left of the index i in the array nums.
+# If there is no such element, leftSum[i] = 0.
+# rightSum[i] is the sum of elements to the right of the index i in the array nums.
+# If there is no such element, rightSum[i] = 0.
+
+nums = [10, 4, 8, 3]
+answer = []
+for i in range(len(nums)):
+    answer.append(abs(sum(nums[:i]) - sum(nums[i+1:])))
+
+print(answer)
