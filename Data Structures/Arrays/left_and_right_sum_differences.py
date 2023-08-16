184da515c618bc1ef6cb2cff21bfc14fd45852f5
Nair, Anuj <anujnairr@gmail.com>
2023-08-16 21:38:53 +0530

    Left and right sum differences in an array.

--- Data Structures/Arrays/left_and_right_sum_differences.py
+++ Data Structures/Arrays/left_and_right_sum_differences.py
@@ -5,7 +5,6 @@
 # If there is no such element, leftSum[i] = 0.
 # rightSum[i] is the sum of elements to the right of the index i in the array nums.
 # If there is no such element, rightSum[i] = 0.
-
 nums = [10, 4, 8, 3]
 answer = []
 for i in range(len(nums)):
