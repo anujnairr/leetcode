2a034e90f12bcbaabf7ffce9fc294f964f747e54
Nair, Anuj <anujnairr@gmail.com>
2023-09-11 14:24:07 +0530

    To lower case.

--- Data Structures/Strings/to_lower_case.py
+++ Data Structures/Strings/to_lower_case.py
@@ -1,4 +1,3 @@
-# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
-
+# Given a string s, return the string after replacing every uppercase letter with the same lowercaseletter.
 s = "Hello"
 print(s.lower())
