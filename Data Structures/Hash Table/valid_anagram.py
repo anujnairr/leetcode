52fc1aa353f1411154b01f2f7337a135c258d773
Nair, Anuj <anujnairr@gmail.com>
2023-09-22 00:59:31 +0530

    Valid anagram

---
+++ Data Structures/Hash Table/valid_anagram.py
@@ -0,0 +1,32 @@
+# Valid Anagram
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+from collections import defaultdict, Counter
+s = "anagram"
+t = "nagaram"
+s, t = "rat", "car"
+s, t = "aa", "a"
+
+ans1 = defaultdict(int)
+ans2 = defaultdict(int)
+for i in s:
+    if i in ans1:
+        ans1[i] += 1
+    else:
+        ans1[i] = 1
+
+for i in t:
+    if i in ans2:
+        ans2[i] += 1
+    else:
+        ans2[i] = 1
+
+if ans1 == ans2:
+    print(True)
+
+
+# ans1 = dict(Counter(s))
+# ans2 = dict(Counter(t))
+# if ans1 == ans2:
+#     return True
