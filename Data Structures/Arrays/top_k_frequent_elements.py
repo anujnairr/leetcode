85b82705f91181cb4fcf30f925de799a55ea4949
Nair, Anuj <anujnairr@gmail.com>
2023-10-04 00:28:40 +0530

    Top K frequent elements (Neetcode's method)

--- Data Structures/Arrays/top_k_frequent_elements.py
+++ Data Structures/Arrays/top_k_frequent_elements.py
@@ -1,47 +1,32 @@
 # Top K Frequent Elements
 # Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+
 nums, k = [1, 1, 1, 2, 2, 3], 2
-nums, k = [1, 2], 2
+# nums, k = [1, 2], 2
 # nums, k = [3, 0, 1, 0], 1
 
-# m = {}
-# ans = []
-# for i in nums:
-#     if i in m:
-#         m[i] += 1
-#     else:
-#         m[i] = 1
+ans = [[] for x in range(len((nums))+1)]
 
-# # value_array = []
-# # for key, value in m.items():
-# #     value_array.append(value)
-# # print(value_array)
+m = {}
+for i in nums:
+    if i not in m:
+        m[i] = 1
+    else:
+        m[i] += 1
 
-# print(f"my dictionary is {m}")
-# i = 1
-# while i <= k:
-#     ans.append("Yes")
-#     i += 1
+for key, value in m.items():
+    ans[value].append(key)
 
-# print(f"my answer is {ans}")
+arr = []
+result = []
+for j in range(len(ans)-1, 0, -1):
+    if len(ans[j]) != 0 and len(result) < k:
+        result += ans[j]
+    #     for l in ans[j]:
+    #         arr.append(l)
 
+    # if len(arr) == k:
+    #     print(arr)
 
-count = {}
-freq = [[] for i in range(len(nums) + 1)]
-print(freq)
-
-for n in nums:
-    count[n] = 1 + count.get(n, 0)
-for n, c in count.items():
-    freq[c].append(n)
-    print(freq)
-
-res = []
-for i in range(len(freq) - 1, 0, -1):
-    for n in freq[i]:
-        res.append(n)
-        print(res)
-        if len(res) == k:
-            print(res)
-
-        # O(n)
+print(result)
