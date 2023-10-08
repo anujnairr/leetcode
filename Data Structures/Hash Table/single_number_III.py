1e9f52be94baa70e763f762fdaa88e6036329f77
Nair, Anuj <anujnairr@gmail.com>
2023-10-08 06:59:22 +0530

    Single Number III

---
+++ Data Structures/Hash Table/single_number_III.py
@@ -0,0 +1,21 @@
+# Single Number III
+# Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+# Find the two elements that appear only once.
+# You can return the answer in any order.
+# You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+nums = [1, 2, 1, 3, 2, 5]
+nums = [-1, 0]
+
+m = {}
+for i in nums:
+    if i not in m:
+        m[i] = 1
+    else:
+        m[i] += 1
+
+ans = []
+for key, value in m.items():
+    if value == 1:
+        ans.append(key)
+
+print(ans)
