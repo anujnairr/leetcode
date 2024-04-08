4a3139d4b2683f513c351f1b16395822e31f3a44
Nair, Anuj <anujnairr@gmail.com>
2024-04-08 19:57:43 +0530

    Day 9

---
+++ Javascript 30 Day Challenge/Day 9 sleep.js
@@ -0,0 +1,19 @@
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds.
+// It can resolve any value
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((res, reject) => {
+        setTimeout(() => {
+            res()
+        }, millis)
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
